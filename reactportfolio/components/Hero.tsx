1d49f287501bfce904804d98b7bdff327e6d23fe
Armel Bogue <komlanbogue@gmail.com>
2024-06-28 23:06:26 +0000

    "Added MagicButton component and updated Hero component with new paragraph and anchor tag"

--- reactportfolio/components/Hero.tsx
+++ reactportfolio/components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
   return (
@@ -24,7 +26,17 @@ const Hero = () => {
             words='Transforming Concepts into Seamless Experiences'
           />
 
-          
+          <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+            Hi, I&apos;m Armel, a Web Developper based in Togo.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title='Show my work'
+              icon={<FaLocationArrow />}
+              position='right'
+              />
+          </a>
 
         </div>
       </div>
